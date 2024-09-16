a19548c4c23b65cf82e702d09ffbe90b442cedf1
Daniel Zairin <danielzairink@gmail.com>
2024-09-16 09:19:52 +0800

    add bin file

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
 import fs from "node:fs";
-
 /** @typedef {import("./types.ts").Quote} Quote */
-
 const PATH_TO_QUOTES = "quotes.json";
-
 const rawQuotes = fs.readFileSync(PATH_TO_QUOTES);
-
 /** @type {Array<Quote>} */
 const quotes = JSON.parse(rawQuotes.toString());
-
 /**
  * @returns {Quote} A random quote
  */
 function getRandomQuote() {
-  return quotes[Math.floor(Math.random() * quotes.length)];
+    return quotes[Math.floor(Math.random() * quotes.length)];
 }
-
 console.log(getRandomQuote());
